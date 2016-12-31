d0f502765585f1a215a5ea6fbd3c39dbd155443a
gl_zwp <gl_zwp@hotmail.com>
2016-12-31 11:01:50 +0800

    Define routes and build basic controllers

--- express_loc8r/app_server/controllers/main.js
+++
@@ -1,4 +0,0 @@
-﻿/* GET home page */
-module.exports.index = function (req, res) {
-    res.render('index', { title: 'Express' });
-};
---
+++ express_loc8r/app_server/controllers/others.js
@@ -0,0 +1,4 @@
+﻿/* GET home page */
+module.exports.about = function (req, res) {
+    res.render('index', { title: 'About' });
+};
