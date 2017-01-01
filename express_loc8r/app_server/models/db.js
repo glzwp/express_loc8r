0bca2bcb100a8c3365121cf905defa2b65d343da
gl_zwp <gl_zwp@hotmail.com>
2017-01-01 19:22:56 +0800

    Setup mogoose

---
+++ express_loc8r/app_server/models/db.js
@@ -0,0 +1,45 @@
+﻿var mongoose = require('mongoose');
+var readLine = require("readline");
+if (process.platform === "win32") {
+    var rl = readLine.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.on("SIGINT", function () {
+        process.emit("SIGINT");
+    });
+}
+
+var dbURI = 'mongodb://root:wGi1XAJH@104.198.217.66:27017/Loc8r';
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose disconnected');
+});
+
+gracefulShutdown = function (msg, callback) {
+    mongoose.connection.close(function () {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+};
+
+// For app termination
+process.on('SIGINT', function () {
+    gracefulShutdown('app termination', function () {
+        process.exit(0);
+    });
+});
+
+// For Heroku app termination
+process.on('SIGTERM', function () {
+    gracefulShutdown('Heroku app shutdown', function () {
+        process.exit(0);
+    });
+});
