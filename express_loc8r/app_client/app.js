fdd1a238eb140cc1f2bcac022d7a412e6dbcef88
gl_zwp <gl_zwp@hotmail.com>
2017-01-02 22:50:58 +0800

    Adding controller to a route

--- express_loc8r/app_client/app.js
+++ express_loc8r/app_client/app.js
@@ -3,7 +3,8 @@
 function config($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl: 'home/home.view.html'
+            templateUrl: 'home/home.view.html',
+            controller: 'homeCtrl'
         })
         .otherwise({ redirectTo: '/' });
 }
