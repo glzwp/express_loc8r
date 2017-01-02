ba9812d566306ad6756603c6c0e0404285cc07bd
gl_zwp <gl_zwp@hotmail.com>
2017-01-03 00:01:15 +0800

    Using filters and directives

---
+++ express_loc8r/app_client/common/directive/ratingStars/ratingStars.directive.js
@@ -0,0 +1,12 @@
+﻿angular
+    .module('loc8rApp')
+    .directive('ratingStars', ratingStars);
+function ratingStars() {
+    return {
+        restrict: 'EA',
+        scope: {
+            thisRating: '=rating'
+        },
+        templateUrl: '/common/directives/ratingStars/ratingStars.template.html'
+    };
+}
