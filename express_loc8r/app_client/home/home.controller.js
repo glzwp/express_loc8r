83db456239ec69915a710b90039a0dc87cab58bc
gl_zwp <gl_zwp@hotmail.com>
2017-01-02 23:06:16 +0800

    Using controllerAs syntax

--- express_loc8r/app_client/home/home.controller.js
+++ express_loc8r/app_client/home/home.controller.js
@@ -1,12 +1,13 @@
 ﻿angular
     .module('loc8rApp')
     .controller('homeCtrl', homeCtrl);
-function homeCtrl($scope) {
-    $scope.pageHeader = {
+function homeCtrl() {
+    var vm = this; //Angular uses 'this' inside the function, and binds it to $scope. Store 'this' in vm. We can access vm instead of the scope
+    vm.pageHeader = {
         title: 'Loc8r',
         strapline: 'Find places to work with wifi near you!'
     };
-    $scope.sidebar = {
+    vm.sidebar = {
         content: "Looking for wifi and a seat etc etc"
     };
 }
