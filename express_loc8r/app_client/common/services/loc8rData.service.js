09d681111c6b13443c42daf3abf7e61873d83731
gl_zwp <gl_zwp@hotmail.com>
2017-01-03 10:49:58 +0800

    Manually injecting dependencies to protect against minification

--- express_loc8r/app_client/common/services/loc8rData.service.js
+++ express_loc8r/app_client/common/services/loc8rData.service.js
@@ -3,6 +3,7 @@
     angular
         .module('loc8rApp')
         .service('loc8rData', loc8rData);
+    loc8rData.$inject = ['$http'];
     function loc8rData($http) {
         var locationByCoords = function (lat, lng) {
             return $http.get('/api/locations?lng=' + lng + '&lat=' + lat +
--- express_loc8r/app_client/home/home.controller.js
+++ express_loc8r/app_client/home/home.controller.js
@@ -2,6 +2,7 @@
     angular
         .module('loc8rApp')
         .controller('homeCtrl', homeCtrl);
+    homeCtrl.$inject = ['$scope', 'loc8rData', 'geolocation'];
     function homeCtrl($scope, loc8rData, geolocation) {
         var vm = this; //Angular uses 'this' inside the function, and binds it to $scope. Store 'this' in vm. We can access vm instead of the scope
         vm.pageHeader = {
