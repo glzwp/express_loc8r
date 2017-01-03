e366411dc3a2cac99691b7664fdb74d6e283b539
gl_zwp <gl_zwp@hotmail.com>
2017-01-03 13:11:22 +0800

    Creating an isolated HTML host page. Making reusable page framework directives

---
+++ express_loc8r/app_client/common/directives/footerGeneric/footerGeneric.directive.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    angular
+        .module('loc8rApp')
+        .directive('footerGeneric', footerGeneric);
+    function footerGeneric() {
+        return {
+            restrict: 'EA',
+            templateUrl: '/common/directives/footerGeneric/footerGeneric.template.html'
+        };
+    }
+})();
