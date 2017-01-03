f2f58fde86f83a6d6efdad6d51c8a9b9fe206e79
gl_zwp <gl_zwp@hotmail.com>
2017-01-03 16:48:21 +0800

    Using URL parameters in controllers and services

---
+++ express_loc8r/app_client/locationDetail/locationDetail.controller.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    angular
+        .module('loc8rApp')
+        .controller('locationDetailCtrl', locationDetailCtrl);
+    locationDetailCtrl.$inject = ['$routeParams', 'loc8rData'];
+    function locationDetailCtrl($routeParams, loc8rData) {
+        var vm = this;
+        vm.locationid = $routeParams.locationid;
+        loc8rData.locationById(vm.locationid)
+            .then(function mySucces(response) {
+                vm.data = { location: response.data };
+                vm.pageHeader = {
+                    title: vm.data.location.name
+                };
+            }, function myError(response) {
+                console.log(response);
+            }
+            );
+    }
+})();
