047b1406425ce1e479f839fe7990e01c55edbeed
gl_zwp <gl_zwp@hotmail.com>
2017-01-03 10:43:04 +0800

    Wrapping each file for IIFE

--- express_loc8r/app_client/home/home.controller.js
+++ express_loc8r/app_client/home/home.controller.js
@@ -1,37 +1,39 @@
-﻿angular
-    .module('loc8rApp')
-    .controller('homeCtrl', homeCtrl);
-function homeCtrl($scope,loc8rData, geolocation) {
-    var vm = this; //Angular uses 'this' inside the function, and binds it to $scope. Store 'this' in vm. We can access vm instead of the scope
-    vm.pageHeader = {
-        title: 'Loc8r',
-        strapline: 'Find places to work with wifi near you!'
-    };
-    vm.sidebar = {
-        content: "Looking for wifi and a seat etc etc"
-    };
-    vm.message = "Checking your location";
-    vm.getData = function (position) {
-        var lat = position.coords.latitude,
-            lng = position.coords.longitude;
-        vm.message = "Searching for nearby places";
-        loc8rData.locationByCoords(lat, lng)
-            .then(function mySucces(response) {
-                vm.message = response.data.length > 0 ? "" : "No locations found";
-                vm.data = { locations: response.data };
-            }, function myError(response) {
-                vm.message = "Sorry, something's gone wrong";
+﻿(function () {
+    angular
+        .module('loc8rApp')
+        .controller('homeCtrl', homeCtrl);
+    function homeCtrl($scope, loc8rData, geolocation) {
+        var vm = this; //Angular uses 'this' inside the function, and binds it to $scope. Store 'this' in vm. We can access vm instead of the scope
+        vm.pageHeader = {
+            title: 'Loc8r',
+            strapline: 'Find places to work with wifi near you!'
+        };
+        vm.sidebar = {
+            content: "Looking for wifi and a seat etc etc"
+        };
+        vm.message = "Checking your location";
+        vm.getData = function (position) {
+            var lat = position.coords.latitude,
+                lng = position.coords.longitude;
+            vm.message = "Searching for nearby places";
+            loc8rData.locationByCoords(lat, lng)
+                .then(function mySucces(response) {
+                    vm.message = response.data.length > 0 ? "" : "No locations found";
+                    vm.data = { locations: response.data };
+                }, function myError(response) {
+                    vm.message = "Sorry, something's gone wrong";
+                });
+        };
+        vm.showError = function (error) {
+            $scope.$apply(function () {
+                vm.message = error.message;
             });
-    };
-    vm.showError = function (error) {
-        $scope.$apply(function () {
-            vm.message = error.message;
-        });
-    };
-    vm.noGeo = function () {
-        $scope.$apply(function () {
-            vm.message = "Geolocation is not supported by this browser.";
-        });
-    };
-    geolocation.getPosition(vm.getData, vm.showError, vm.noGeo);
-}
+        };
+        vm.noGeo = function () {
+            $scope.$apply(function () {
+                vm.message = "Geolocation is not supported by this browser.";
+            });
+        };
+        geolocation.getPosition(vm.getData, vm.showError, vm.noGeo);
+    }
+})();
