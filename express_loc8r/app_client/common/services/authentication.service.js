cc35e5bcf45d3603871317d716bd13dfc71603a5
gl_zwp <gl_zwp@hotmail.com>
2017-01-04 21:40:55 +0800

    Allowing users to sign up, sign in, and sign out

--- express_loc8r/app_client/common/services/authentication.service.js
+++ express_loc8r/app_client/common/services/authentication.service.js
@@ -10,9 +10,35 @@
         var getToken = function () {
             return $window.localStorage['loc8r-token'];
         };
+
+        register = function (user) {
+            return $http.post('/api/register', user)
+                .then(function mySucces(response) {
+                    saveToken(response.data.token);
+                }, function myError(response) {
+                console.log(response);
+            });
+        };
+
+        login = function (user) {
+            return $http.post('/api/login', user)
+                .then(function mySucces(response) {
+                saveToken(response.data.token);
+            }, function myError(response) {
+                console.log(response);
+            });
+        };
+
+        logout = function () {
+            $window.localStorage.removeItem('loc8r-token');
+        };
+
         return {
             saveToken: saveToken,
-            getToken: getToken
+            getToken: getToken,
+            register: register,
+            login: login,
+            logout: logout
         };
     }
 })();
