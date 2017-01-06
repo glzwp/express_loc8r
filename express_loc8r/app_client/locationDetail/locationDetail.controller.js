7d4fa7b7b0b7e46429e51297e6f1f8e1b577bd5e
gl_zwp <gl_zwp@hotmail.com>
2017-01-06 21:45:00 +0800

    SHOWING DIFFERENT BUTTONS BASED ON THE CURRENT USER’S STATUS

--- express_loc8r/app_client/locationDetail/locationDetail.controller.js
+++ express_loc8r/app_client/locationDetail/locationDetail.controller.js
@@ -2,10 +2,13 @@
     angular
         .module('loc8rApp')
         .controller('locationDetailCtrl', locationDetailCtrl);
-    locationDetailCtrl.$inject = ['$routeParams', '$uibModal', 'loc8rData'];
-    function locationDetailCtrl($routeParams, $uibModal, loc8rData) {
+    locationDetailCtrl.$inject = ['$routeParams', '$location', '$uibModal', 'loc8rData', 'authentication'];
+    function locationDetailCtrl($routeParams, $location, $uibModal, loc8rData,authentication) {
         var vm = this;
         vm.locationid = $routeParams.locationid;
+        vm.isLoggedIn = authentication.isLoggedIn();
+        vm.currentPath = $location.path();
+
         loc8rData.locationById(vm.locationid)
             .then(function mySucces(response) {
                 vm.data = { location: response.data };
